fa64ae4f7a29079b2abe534107204746b74e269e
XI-xi-Sakura <1959701844@qq.com>
2025-05-05 18:42:08 +0800

    Log

---
+++ Linux_OperatingSystem/20.线程池/Log/main.cc
@@ -0,0 +1,21 @@
+#include "Log.hpp"
+
+using namespace LogMudule;
+
+int main()
+{
+    ENABLE_FILE_LOG();
+
+    LOG(LogLevel::DEBUG) << "hello file";
+    LOG(LogLevel::DEBUG) << "hello file";
+    LOG(LogLevel::DEBUG) << "hello file";
+    LOG(LogLevel::DEBUG) << "hello file";
+
+
+    ENABLE_CONSOLE_LOG();
+    LOG(LogLevel::DEBUG) << "hello world";
+    LOG(LogLevel::DEBUG) << "hello world";
+    LOG(LogLevel::DEBUG) << "hello world";
+    LOG(LogLevel::DEBUG) << "hello world";
+    return 0;
+}
